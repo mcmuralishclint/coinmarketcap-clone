925ee1af6ed869540a517f5ec70ad8dd884f21c3
Muralish Clinton <muralishc@Muralish-C-L.zone24x7.lk>
2021-08-20 16:10:08 +0530

    single coin pageinit

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -10,4 +10,15 @@ router.get('/',(req,res)=>{
     res.render('../templates/index',{user:user,coins:coins})
 })
 
+router.get('/:id',(req,res)=>{  
+    var coin = coins.filter(function(item) {
+        return item.id == req.params.id;
+      });
+      if(coin.length>0  ){
+        res.render('../templates/single_coin',{coin:coin[0]})
+      }else{
+          res.send("The coin you're searching for is not in our database")
+      }    
+})
+
 module.exports = router
