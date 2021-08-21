498060f25aed4d07e3d8bebb83403475466b4e39
Muralish Clinton <muralishc@Muralish-C-L.zone24x7.lk>
2021-08-21 15:47:05 +0530

    create coin

--- routes/coinRouter.js
+++ routes/coinRouter.js
@@ -8,14 +8,15 @@ mongoose.connect(process.env.CONNECTION_STRING, {useNewUrlParser: true, useUnifi
 //Get the default connection
 var db = mongoose.connection;
 
-router.get('/',(req,res)=>{
-    const coins = getCoins()
+router.get('/',async(req,res)=>{
+    const coins = await Coin.find({})
     res.send(coins)
 })
 
-router.post('/',(req,res)=>{
-    const coins = Coin.find({name: req.body.name})
-    if (coins){
+router.post('/',async(req,res)=>{
+    var coin = await Coin.findOne({name: req.body.name})
+
+    if (coin){
         return res.send(req.body.name + ' already exists')
     }
     var coin = new Coin({
@@ -23,17 +24,11 @@ router.post('/',(req,res)=>{
         ticker: req.body.ticker
     })
     try{
-        var result = coin.save()
-        res.send(req.body.name + ' created')
+        var result = await coin.save()
+        res.send(result)
     }catch(err){
         res.send(err)
     }
 })
-
-async function getCoins(){
-    const coins = await Coin.find({})
-    console.log("Finding coins..." + coins)
-    return coins
-}
 
 module.exports = router
